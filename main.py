8a7012d8f8c450abae305ce618393d95ec8d5221
Fyz1408 <daniellykke1@gmail.com>
2023-12-06 13:23:22 +0100

    Training the model and getting accuracy

--- main.py
+++ main.py
@@ -5,18 +5,58 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    fashion_mnist = tf.keras.datasets.fashion_mnist
-    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-    print('Tensor flow version: ' + tf.__version__)
-
+# Display a image from the given index
+def displayImage(index):
     plt.figure()
-    plt.imshow(train_images[0])
+    plt.imshow(train_images[index])
     plt.colorbar()
     plt.grid(False)
     plt.show()
 
+
+# Display the first 25 images of the data set
+def trainingSet():
+    plt.figure(figsize=(10, 10))
+    for i in range(25):
+        plt.subplot(5, 5, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[i]])
+    plt.show()
+
+
+if __name__ == '__main__':
+    print('Tensor flow version: ' + tf.__version__)
+
+    fashion_mnist = tf.keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
     class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+    # Build the model and set up the layers
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
+
+    # Compile the model
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+
+    # Train the model
+    model.fit(train_images, train_labels, epochs=10)
+
+    # Evaluate accuracy
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+    print('\nTest accuracy:', test_acc)
